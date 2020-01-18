d923421a9d1844363c76ff9f71cd8ba3c7d3e770
Jack Markwalter <jackmarkwalter3@gmail.com>
2020-01-18 11:49:49 -0500

    counter function

--- script.js
+++ script.js
@@ -8,10 +8,30 @@ var city = "Atlanta";
 //Then pull a 5-day forecast for the same city
 
 //Then save all of that information to local storage
-
+//Need to save city to local storage to show on sideBar
 //Then pull of that information from local storage to display onclick
 
-
+counter = 0;
+function setCounter() {
+    var test = localStorage.getItem("city0");
+    if(test === "") {
+        for (var i = 0; i < 5; i++) {
+            localStorage.setItem("city" + i, "");
+    }
+        counter = 0;
+    }
+    else {
+        for (var i = 0; i < 5; i++) {
+            test = localStorage.getItem("city" + i);
+            if(test === "") {
+                counter = i;
+            }
+            else if (counter === 4) {
+                counter = 0;
+            }
+        }
+    }
+};
 var qURLFirst = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&APPID=" + ApiKey;
 
 $.ajax({
@@ -22,7 +42,8 @@ $.ajax({
     console.log(response.main.temp);
     console.log(response.main.humidity);
     console.log(response.wind.speed);
-    
+    console.log(city);
+
     var temp = response.main.temp;
     var humidity = response.main.humidity;
     var windSpeed = response.wind.speed;
@@ -30,7 +51,7 @@ $.ajax({
     localStorage.setItem(city + " tempCurrent", temp);
     localStorage.setItem(city + " humidityCurrent", humidity);
     localStorage.setItem(city + " windSpeedCurrent", windSpeed);
-
+    localStorage.setItem("city" + counter, city);
 
     var lat = response.coord.lat;
     var lon = response.coord.lon;
@@ -57,7 +78,12 @@ $.ajax({
             method: "GET"
         }).then(function(response3) {
             console.log(response3);
-            
+            if(counter === 4) {
+                counter = 0;
+            }
+            else {
+                counter++;
+            };
             for(var i = 0; i < 5; i++) {
                 console.log(response3.list[i].main.temp);
                 console.log(response3.list[i].main.humidity);
